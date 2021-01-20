d65def7ffa0a152f2d96c7d430431286b7f1494b
igoreta <ivoronline@gmail.com>
2021-01-20 08:52:11 +0100

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_security_expression_preauthorized/SpringbootSecurityExpressionPreauthorizedApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_security_expression_preauthorized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityExpressionPreauthorizedApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootSecurityExpressionPreauthorizedApplication.class, args);
+  }
+
+}
---
+++ src/main/java/com/ivoronline/springboot_security_expression_preauthorized/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.ivoronline.springboot_security_expression_preauthorized.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+    //SPECIFY ACCESS TO ENDPOINTS
+    httpSecurity.authorizeRequests()
+      .antMatchers("/endPoint1").denyAll()     //No access (even after log in)
+      .antMatchers("/endPoint2").permitAll();  //No log in (anonymous access)
+
+    //REDIRECT TO LOGIN FORM
+    httpSecurity.formLogin();
+
+  }
+
+}
